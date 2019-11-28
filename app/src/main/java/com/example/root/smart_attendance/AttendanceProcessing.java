3828ea88869a19d08a43873cb545451d7d5ca253
joyroy95 <joyroylightoj@gmail.com>
2019-11-28 21:25:55 +0600

    Smart Attendance System

---
+++ app/src/main/java/com/example/root/smart_attendance/AttendanceProcessing.java
@@ -0,0 +1,20 @@
+package com.example.root.smart_attendance;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+/**
+ * Created by root on 8/1/17.
+ */
+
+public class AttendanceProcessing extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atten_processing);
+    }
+}
